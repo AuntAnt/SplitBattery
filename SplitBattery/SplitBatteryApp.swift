4db8305f603d0151113fa7b938544f943df296f2
Anton Kuzmin <furthertwoant@gmail.com>
2025-09-25 21:28:05 +0400

    Initial Commit

---
+++ SplitBattery/SplitBatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBatteryApp.swift
+//  SplitBattery
+//
+//  Created by Anton Kuzmin on 25.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SplitBatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
